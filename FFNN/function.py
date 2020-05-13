cadffd4942de98421537d8c5c88d9432a52d68cc
Diwas Bhattarai <diwas.bhattarai@web.de>
2020-05-13 16:01:59 +0200

    print the updated test and train results

--- FFNN/function.py
+++ FFNN/function.py
@@ -1,4 +1,6 @@
 import torch
+import sys
+import time
 
 class FFNNTraintest():
 
@@ -21,6 +23,8 @@ class FFNNTraintest():
         self.network.train()
         running_loss = 0.0
         total = 0
+        total_time_required = 0.0
+        start = time.time()
         for inputs, targets in trainset:
             inputs = inputs.to(self.device)
             targets = targets.to(self.device)
@@ -31,29 +35,46 @@ class FFNNTraintest():
             self.optimizer.step()
             running_loss += l.item()
             total += targets.size(0)
-        return total, running_loss
-       
+            end = time.time()
+            time_required = (end-start)
+            total_time_required += time_required
+            self.__print(time_required, total_time_required, total, len(trainset.dataset))
+            start = time.time()
+        return running_loss
 
     def test(self, testset):
         self.network.eval()
         correct = 0
         total = 0
+        running_loss = 0.0
         with torch.no_grad():
+            total_time_required = 0.0
+            start = time.time()
             for inputs, targets in testset:
                 inputs = inputs.to(self.device)
                 targets = targets.to(self.device)
                 outputs = self.__run(inputs)
+                l = self.loss(outputs, targets)
+                running_loss += l.item()
                 _, predicted = torch.max(outputs.data, 1)
                 total += targets.size(0)
                 correct += (predicted == targets).sum().item()
-        return total, correct
+                end = time.time()
+                time_required = (end-start)
+                total_time_required += time_required
+                self.__print(time_required, total_time_required, total, len(testset.dataset))
+                start = time.time()
+        return correct, running_loss
     
     def __run(self, inputs):
         inputs = self._resize(inputs)
         outputs = self.network(inputs)
         return outputs
 
-
     def _resize(self, inputs):
         return inputs.reshape(1,-1)
+
+    def __print(self, time_required, total_time_required, total, num_samples):
+        outstr = 'Time required for last sample: {:.2f}sec. Total time: {:.2f}sec.  Total tests: {}/{}'.format(time_required, total_time_required, total, num_samples)
+        sys.stdout.write('\r'+ outstr)
 
--- FFNN/module.py
+++ FFNN/module.py
@@ -5,22 +5,31 @@ import numpy as np
 import torch.nn.functional as F
 
 class FFNN(nn.Module):
-    def __init__(self, in_features=3, out_features=2, bias=False, drop_p=0.5, fcout=[256, 128]):
+    def __init__(self, in_features, out_features, drop_p, fcout, bias=False):
         super().__init__()
-        self.in_features = in_features
+        self.in_features = int(in_features)
         self.out_features = out_features
         self.drop_p = drop_p
         self.fcout = fcout 
         self.bias = bias
-        self.fc1 = nn.Linear(self.in_features, self.fcout[0], bias)
-        self.fc2 = nn.Linear(self.fcout[0], self.fcout[1], bias)
-        self.fc3 = nn.Linear(self.fcout[1], self.out_features, bias)
+
+        if self.drop_p is None:
+            self.drop_p = 0.4
+        if self.fcout is None:
+            self.fcout = [256, 128]
+
+        # Weights are initialized based on Kaiming Initialization.
+        # weights = random weights * squarerootof(2/numberoffeature)
+        # variance = 2/numberoffeatures
+        self.fc1 = nn.Linear(self.in_features, self.fcout[0], self.bias)
+        self.fc2 = nn.Linear(self.fcout[0], self.fcout[1], self.bias)
+        self.fc3 = nn.Linear(self.fcout[1], self.out_features, self.bias)
         self.relu = nn.ReLU(inplace=True)
         self.drop = nn.Dropout(self.drop_p)
 
     def forward(self, x):
         outputs = F.relu(self.fc1(x))
-        outputs = F.relu(self.fc2(outputs))
         outputs = self.drop(outputs)
+        outputs = F.relu(self.fc2(outputs))
         outputs = self.fc3(outputs)
         return outputs
