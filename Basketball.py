a0ac4ec1de9ea10c02d671b6d6ce8c84f3fdd2a1
Diwas Bhattarai <diwas.bhattarai@web.de>
2020-05-13 15:59:02 +0200

    pass width and height of the frames.
    new print() format

--- Basketball.py
+++ Basketball.py
@@ -16,13 +16,18 @@ import copy
 import torch.nn as nn
 
 class Basketball():
-    def __init__(self, data, num_frame, split='training'):
+    def __init__(self, data, width=50, height=50, num_frames=100, split='training'):
         super().__init__()
-        if not data or not num_frame:
+        if not data or not num_frames:
             raise RuntimeError('Please pass parameter.')
         self.data = data
-        self.num_frame = num_frame
-        dataset = Dataset.Basketball(self.data, split=split, num_frame = self.num_frame)
+        self.width = width
+        self.height = height
+        self.num_frames = num_frames
+        self.channel = 3
+        self.drop_p = 0.4
+        self.out_features = 2                                        # only 2 classifier hit or miss
+        dataset = Dataset.Basketball(self.data, split=split, num_frames = self.num_frames)
         self.trainset, self.testset = dataset.train_test_split(train_size=0.8)
         self.trainset_loader = DataLoader(self.trainset, shuffle=True)
         self.testset_loader = DataLoader(self.testset, shuffle=True)
@@ -31,6 +36,7 @@ class Basketball():
         torch.backends.cudnn.benchmark = True
 
     def run(self, module='FFNN', testeverytrain=True, EPOCHS=1, opticalpath=None):
+        print("Starting {} using the data in {}.".format(module, self.data ) )
         if module=='FFNN':
             obj = self.__FFNN()
         elif module=='CNN3D':
@@ -41,34 +47,31 @@ class Basketball():
             obj, train_optical_loader, test_optical_loader = self.__OPTICALCONV3D(opticalpath)
         else:
             ValueError()
-
-        print("------------------------------------------------------")
-        print("|    EPOCHS  |   Total   |   Loss    |   Accuracy    |")
-        print("------------------------------------------------------")
         i = 1
         for epoch in range(0, EPOCHS):
-            total = 0
             running_loss = 0
+            total_train = len(self.trainset_loader.dataset)
+            total_test = len(self.testset_loader.dataset)
+            print('Epocs: ', i)
             if module=='OPTICALCONV3D':
-                total, running_loss = obj.train(self.trainset_loader, train_optical_loader)
+                running_loss = obj.train(self.trainset_loader, train_optical_loader)
             else:
-                total, running_loss = obj.train(self.trainset_loader)
-            print("|      {}     |   {}     |   {:.4f}  |               |".format(i, total, running_loss/total) )
-            print("------------------------------------------------------")
+                running_loss = obj.train(self.trainset_loader)
+            print("\nTrain loss: {:.4f}".format(running_loss/total_train) )
             if  (i == EPOCHS) or testeverytrain:
                 if module=='OPTICALCONV3D':
-                    total, correct = obj.test(self.testset_loader, test_optical_loader)
+                    correct, test_loss = obj.test(self.testset_loader, test_optical_loader)
                 else:
-                    total, correct = obj.test(self.testset_loader)
-                print("|      {}     |    {}     |           | ({}) {:.1f} %   |".format(i, total, correct, 100 * correct/total))
-                print("------------------------------------------------------")
+                    correct, test_loss = obj.test(self.testset_loader)
+                print("\nTest loss: {:.4f}, Prediction: ({}/{}) {:.1f} %".format(test_loss/total_test, correct, total_test, 100 * correct/total_test))
+            print("-------------------------------------")
             i += 1
 
     def __FFNN(self):
         loss = torch.nn.CrossEntropyLoss().to(self.device)
-        out_features = 2                                        # only 2 classifier hit or miss
+        #in_features = self.num_frames * self.channel * self.height * self.width
         in_features = self.trainset_loader.dataset[0][0].numel()
-        network = FFNN.module.FFNN(in_features=in_features, out_features=out_features)
+        network = FFNN.module.FFNN(in_features=in_features, out_features=self.out_features, drop_p=0.4, fcout = [256, 128])
         if torch.cuda.device_count() > 1:                       # will use multiple gpu if available
             network = nn.DataParallel(network) 
         network.to(self.device)
@@ -78,7 +81,7 @@ class Basketball():
 
     def __CNN3D(self):
         loss = torch.nn.CrossEntropyLoss().to(self.device)
-        network = CNN3D.module.CNN3D(width=2*48, height=48, in_channels=3, out_features=2, drop_p=0.2, fc1out=256, fc2out=128, frames=100) # the shape of input will be Batch x Channel x Depth x Height x Width
+        network = CNN3D.module.CNN3D(width=self.width, height=self.height, in_channels=self.channel, num_frames=self.num_frames, out_features=self.out_features, drop_p=self.drop_p, fcout=[256, 128]) # the shape of input will be Batch x Channel x Depth x Height x Width
         if torch.cuda.device_count() > 1:   #   will use multiple gpu if available
             network = nn.DataParallel(network) 
         network.to(self.device)
@@ -88,8 +91,10 @@ class Basketball():
 
     def __CNN2DLSTM(self):
         loss = torch.nn.CrossEntropyLoss().to(self.device)
-        encoder2Dnet = CNN2DLSTM.module.CNN2D(width=2*48, height=48, drop_p=0.2, fc1out=256, fc2out=128, frames=100) # Batch x Depth x Channel x Height x Width
-        decoderltsm = CNN2DLSTM.module.LTSM()
+        #width = 384, height=2*192  2 for two views concatenated
+        encoder2Dnet = CNN2DLSTM.module.CNN2D(width=self.width, height=self.width, drop_p=self.drop_p, fcout=[2048, 512], num_frames=self.num_frames) # Batch x Depth x Channel x Height x Width
+        decoderltsm = CNN2DLSTM.module.LTSM(num_frames=self.num_frames, out_features=self.out_features, num_layers=3, hidden_size=256, fc1out=128, drop_p=self.drop_p, bidirectional=True)
+       
         if torch.cuda.device_count() > 1:   #   will use multiple gpu if available
             encoder2Dnet = nn.DataParallel(encoder2Dnet)
             decoderltsm = nn.DataParallel(decoderltsm) 
