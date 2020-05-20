25981b07c8b6f9b7c833a8f102fe772a74b381fd
methesoul <diwas.bhattarai@web.de>
2020-05-20 02:29:25 +0200

    main updated

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-import Basketball
+#!/usr/bin/env python
 
-dp = Basketball.Basketball('cache', 100, None)
-#dp.run('FFNN', testeverytrain=True, EPOCHS=10)
-#dp.run('CNN3D', testeverytrain=True, EPOCHS=10)
-#dp.run('CNN2DLSTM', testeverytrain=True, EPOCHS=10)
-dp.run('OPTICALCONV3D', testeverytrain=True, EPOCHS=1, opticalpath='opticalflow')
+import Basketball
+import serialize
+import time
+
+start_time = time.time()
+data = 'data'
+data48x48 = 'cache'
+num_frames = 96
+split = 'training'
+EPOCHS = 10
+optics = 'optics'
+
+dp = Basketball.Basketball(data, width=128, height=128,  num_frames=num_frames, split=split)
+#dp.run('FFNN', testeverytrain=True, EPOCHS=EPOCHS)
+dp.run('CNN3D', testeverytrain=True, EPOCHS=EPOCHS) #may run on 4gb gpu with 32 Gb RAM
+#dp.run('CNN2DLSTM', testeverytrain=True, EPOCHS=EPOCHS) #may run on 4gb gpu with 32 Gb RAM
+#dp.run('OPTICALCONV3D', testeverytrain=True, EPOCHS=EPOCHS)
+end_time = time.time()
+print("Total time required: {} seconds".format(end_time - start_time))
