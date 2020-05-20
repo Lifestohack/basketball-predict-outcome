81da03a60ad1f9d0893d1a7634b14d758c62ffc8
methesoul <diwas.bhattarai@web.de>
2020-05-20 04:05:55 +0200

    adam instead of sgd
    sgd with lr=0.001 gave around 78.9 max % accuracy
    decreasing sgd didnot give better accuracy
    adam with with lr=0.00001 gave around 82 max % accuracy

--- Basketball.py
+++ Basketball.py
@@ -111,11 +111,11 @@ class Basketball():
 
     def __CNN3D(self):
         loss = torch.nn.CrossEntropyLoss().to(self.device)
-        network = CNN3D.module.CNN3D(width=self.width, height=self.height, in_channels=self.channel, num_frames=self.num_frames, out_features=self.out_features, drop_p=self.drop_p, fcout=[256, 128]) # the shape of input will be Batch x Channel x Depth x Height x Width
+        network = CNN3D.module.CNN3D(width=self.width, height=self.height, in_channels=self.channel, num_frames=self.num_frames, out_features=self.out_features, drop_p=self.drop_p, fcout=[1024]) # the shape of input will be Batch x Channel x Depth x Height x Width
         if torch.cuda.device_count() > 1:                                   #   will use multiple gpu if available
             network = nn.DataParallel(network) 
         network.to(self.device)
-        optimizer = torch.optim.SGD(network.parameters(), lr=0.001, momentum=0.4, nesterov=True)
+        optimizer = torch.optim.Adam(network.parameters(), lr=0.00001)
         obj = CNN3D.function.CNN3DTraintest(self.device, network, loss, optimizer)
         return obj, network
 
