77ade9816e7b36899962fdd6d287f5a39e7a2af8
Diwas Bhattarai <diwas.bhattarai@web.de>
2020-06-28 11:46:06 +0200

    removed unused variables

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 
 import Basketball
-import serialize
 import time
 
 start_time = time.time()
-data = 'data'
-data48x48 = 'cache'
-num_frames = 96
-split = 'training'
-EPOCHS = 10
-optics = 'optics'
+data = 'dataset/data'
+num_frames = 100
+split = 'training' #validation, training
+EPOCHS = 1
 
-dp = Basketball.Basketball(data, width=128, height=128,  num_frames=num_frames, split=split)
+dp = Basketball.Basketball(data, width=224, height=224,  num_frames=num_frames, split=split)
 #dp.run('FFNN', testeverytrain=True, EPOCHS=EPOCHS)
-dp.run('CNN3D', testeverytrain=True, EPOCHS=EPOCHS) #may run on 4gb gpu with 32 Gb RAM
-#dp.run('CNN2DLSTM', testeverytrain=True, EPOCHS=EPOCHS) #may run on 4gb gpu with 32 Gb RAM
+dp.run('CNN3D', testeverytrain=True, EPOCHS=EPOCHS) # EPOCHS = 16  for 30 frames, 20 for 55 frames
+#dp.run('CNN2DLSTM', testeverytrain=True, EPOCHS=EPOCHS) 
 #dp.run('OPTICALCONV3D', testeverytrain=True, EPOCHS=EPOCHS)
 end_time = time.time()
 print("Total time required: {} seconds".format(end_time - start_time))
