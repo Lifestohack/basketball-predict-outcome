68721f17503d74894fd0b76d7bc440513c41569d
diwas <diwas.bhattarai@web.de>
2020-07-02 15:20:04 -0700

    for 128x128 size

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import Basketball
 import time
 
 start_time = time.time()
-data = 'dataset/data'
+data = 'dataset\data\cropped_samples'
 num_frames = 100
 split = 'training' #validation, training
 EPOCHS = 1
 
-dp = Basketball.Basketball(data, width=224, height=224,  num_frames=num_frames, split=split)
+dp = Basketball.Basketball(data, width=128, height=128,  num_frames=num_frames, split=split)
 #dp.run('FFNN', testeverytrain=True, EPOCHS=EPOCHS)
-dp.run('CNN3D', testeverytrain=True, EPOCHS=EPOCHS) # EPOCHS = 16  for 30 frames, 20 for 55 frames
+dp.run('CNN2DLSTM', testeverytrain=True, EPOCHS=EPOCHS) # EPOCHS = 16  for 30 frames, 20 for 55 frames
 #dp.run('CNN2DLSTM', testeverytrain=True, EPOCHS=EPOCHS) 
 #dp.run('OPTICALCONV3D', testeverytrain=True, EPOCHS=EPOCHS)
 end_time = time.time()
