99c804075772a6f6c0f614b8235c9190e0ecfbaf
Diwas Bhattarai <diwas.bhattarai@web.de>
2020-07-05 02:23:59 +0200

    singleton

--- cache.py
+++ cache.py
@@ -1,34 +1,41 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 from psutil import virtual_memory
 
-class Cache():
-    # Window memory management paging doesnot seems to handle this properly. 
-    # After running the training for long time, it starts paging the memory
-    # When it is again needed then it takes long time to retrieve
-    # Run it in a linux operating system if that problem persist
-    def __init__(self):
-        super().__init__()
-        self.__THRESHOLD = 1536 * 1024 * 1024 # in byte
-        self.__length = 0
-        self.__cache = {}
+class Cache(object):
+    class __Cache:
+        def __init__(self):
+            self.__THRESHOLD = 1536 * 1024 * 1024  # in byte
+            self.__length = 0
+            self.__c = {}
 
-    def getcache(self, sample):
-        cache = self.__cache.get(sample)
-        if cache == None:
-            return None, False
-        else:
-            return cache, True
+        def getcache(self, sample):
+            c = self.__c.get(sample)
+            if c == None:
+                return (None, False)
+            else:
+                return (c, True)
 
-    def setcache(self, path, frames, label):
-        mem = virtual_memory().available
-        if mem <= self.__THRESHOLD:
-            return
-        cache = self.__cache.get(path)
-        if cache == None:
-            self.__length += 1
-            value = [frames, label]
-            self.__cache[path] = value 
+        def setcache(self, path, frames, label, ):
+            mem = virtual_memory().available
+            if mem <= self.__THRESHOLD:
+                return
+            c = self.__c.get(path)
+            if c == None:
+                self.__length += 1
+                value = [frames, label]
+                self.__c[path] = value
 
-    def getlength(self):
-        return __length
+        def getlength(self):
+            return __length
+
+    instance = None
+    def __new__(cls): # __new__ always a classmethod
+        if not Cache.instance:
+            Cache.instance = Cache.__Cache()
+        return Cache.instance
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+    def __setattr__(self, name):
+        return setattr(self.instance, name)
+    
