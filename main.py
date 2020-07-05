e14b0db519725e09c10bbdd481dd4a8b8e73660e
Diwas Bhattarai <diwas.bhattarai@web.de>
2020-07-05 05:14:48 +0200

    main file

--- main.py
+++ main.py
@@ -3,16 +3,28 @@
 import Basketball
 import time
 
+# After trainig for 30 frames don't train for higher frames but only first lower frames.
+# So always train higher frames first and then lower frames
 start_time = time.time()
-data = 'dataset\data\cropped_samples'
-num_frames = 100
+data = 'dataset/background/data'    # format should be  "folder1/folder2/folder3"
 split = 'training' #validation, training
-EPOCHS = 1
+dp = Basketball.Basketball(data, width=128, height=128, split=split, trajectory=False)
 
-dp = Basketball.Basketball(data, width=128, height=128,  num_frames=num_frames, split=split)
+EPOCHS = 50
+num_frames = 100
+dp.run(num_frames, 'CNN3D', testeverytrain=True, EPOCHS=EPOCHS)
+
+EPOCHS = 50
+num_frames = 55
+dp.run(num_frames, 'CNN3D', testeverytrain=True, EPOCHS=EPOCHS)
+
+EPOCHS = 50
+num_frames = 30
+dp.run(num_frames, 'CNN3D', testeverytrain=True, EPOCHS=EPOCHS)
+
 #dp.run('FFNN', testeverytrain=True, EPOCHS=EPOCHS)
-dp.run('CNN3D', testeverytrain=True, EPOCHS=EPOCHS)
-dp.run('CNN2DLSTM', testeverytrain=True, EPOCHS=EPOCHS)
+#dp.run('CNN3D', testeverytrain=True, EPOCHS=EPOCHS)
+#dp.run('CNN2DLSTM', testeverytrain=True, EPOCHS=EPOCHS)
 #dp.run('TWOSTREAM', testeverytrain=True, EPOCHS=EPOCHS)
 end_time = time.time()
 print("Total time required: {} seconds".format(end_time - start_time))
